bc7422bf32b74b3dcdee331c1d9f966b2ba6f49d
MOMO KONATE <149674040+momo-konate@users.noreply.github.com>
2025-05-14 17:52:36 +0100

    succes.js
    Update PATH

--- succes.js
+++ succes.js
@@ -7,6 +7,6 @@ const interval = setInterval(() => {
   
   if (countdown <= 0) {
     clearInterval(interval);
-    window.location.href = "http://127.0.0.1:5500/";
+    window.location.href = "https://momo-konate.github.io/portfolioMOMO/";
   }
 }, 1000);
